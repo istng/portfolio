26484561a9b5ebfbee75767545dc0c7be9cc8a62
Iván Gk <ivanstng@gmail.com>
2021-06-01 12:11:55 -0300

    video modal bigger, click close outside video
    autoplay for youtube

--- src/App.vue
+++ src/App.vue
@@ -12,25 +12,4 @@
   text-align: center;
   color: #2c3e50;
 }
-
-.modal {
-  position: absolute;
-  top: 0; right: 0; bottom: 0; left: 0;
-  background-color: rgba(0,0,0,.5);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.modal div {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background-color: white;
-  width: 300px;
-  height: 300px;
-  padding: 5px;
-}
 </style>
--- src/components/Video.vue
+++ src/components/Video.vue
@@ -2,12 +2,10 @@
   <div>
     <img class='video-thumbnail' :src='videoData.thumbnail' @click="openVideo">
     <teleport to="body">
-      <div class="modal" v-if="show">
+      <div class="modal" v-if="show" @click="show = false">
         <div>
-          <iframe :src='youtubeUrl + videoData.urlId' @onClick='other'></iframe>
-          <button @click="show = false">
-            Close
-          </button>
+          <iframe class="video-window" :src='youtubeUrl + videoData.urlId + "?autoplay=1"' @onClick='other'>
+        </iframe>
         </div>
       </div>
     </teleport>
@@ -42,7 +40,27 @@ export default defineComponent({
 </script>
 
 <style type="text/css">
-  .video-thumbnail {
-    width: 100%;
-  }
+.video-thumbnail {
+  width: 100%;
+}
+.video-window{
+   border: 0;
+   height: 85%;
+   position: absolute;
+   width: 85%;
+}
+.modal {
+  position: absolute;
+  top: 0; right: 0; bottom: 0; left: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.modal div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 </style>
