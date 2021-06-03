3bc4585df395b68401a2d14b7d5510a3e00b92da
Iván Gk <ivanstng@gmail.com>
2021-06-03 11:48:53 -0300

    fix button overlay position, change background color, and add thumbnails for shorts

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -3,8 +3,8 @@
     <div class="video-thumbnail-container" @click="openVideo">
       <img class='video-thumbnail' :src='videoData.thumbnail'>
       <img class="play-button" :src='playbutton'>
-      <h4 class="video-title">{{ videoData.title }}</h4>
     </div>
+      <h4 class="video-title">{{ videoData.title }}</h4>
     <teleport to="body">
       <div class="modal" v-if="show" @click="show = false">
         <div>
@@ -47,7 +47,10 @@ export default defineComponent({
 
 <style type="text/css">
 .video-thumbnail {
+  grid-column: 1;
+  grid-row: 1;
   width: 100%;
+  border-radius: 25px;
 }
 .video-title {
   color: #e9a44d;
@@ -55,14 +58,16 @@ export default defineComponent({
 }
 
 .video-thumbnail-container {
+  display: grid;
+  grid-template-rows: repeat(1, 1fr);
   position: relative;
-  display: inline;
 }
 .play-button {
-  position: absolute;
-  bottom: 0;
-  right: 0;
+  grid-column: 1;
+  grid-row: 1;
+  z-index: 1;
   width: 100%;
+  border-radius: 25px;
   opacity: 0;
   background-color: #4B4B4B;
   transition: opacity .25s ease-in-out;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,7 +14,9 @@ import VideoCategory from '@/components/VideoCategory.vue';
 import Footer from '@/components/Footer.vue';
 import nakatomi from '@/assets/nakatomi-drive.png';
 import notshop from '@/assets/not-in-the-shop.png';
-import wip from '@/assets/wip.png';
+import nacherrestre from '@/assets/nacherrestre.png';
+import deshielo from '@/assets/deshielo.png';
+import frutilla from '@/assets/frutilla.png';
 
 export default defineComponent({
   name: 'Home',
@@ -24,7 +26,7 @@ export default defineComponent({
     Footer,
   },
   setup() {
-    const shorts = ref([{thumbnail: wip, urlId: "-K-z-N3I4uI", title: "Nacherrestre"}, {thumbnail: wip, urlId: "Qv8q1ENfWBA", title: "Deshielo Acondicionado"}, {thumbnail: wip, urlId: "j4z3xv2l9_k", title: "Frutilla"}]);
+    const shorts = ref([{thumbnail: nacherrestre, urlId: "-K-z-N3I4uI", title: "Nacherrestre"}, {thumbnail: deshielo, urlId: "Qv8q1ENfWBA", title: "Deshielo Acondicionado"}, {thumbnail: frutilla, urlId: "j4z3xv2l9_k", title: "Frutilla"}]);
     const youtubes = ref([{thumbnail: nakatomi, urlId: "ve8gZ3UEMlU", title: "nakatomi drive"}, {thumbnail: notshop, urlId: "rIZyQmS3L3w", title: "not in the shop"}]);
     return {
       shorts,
