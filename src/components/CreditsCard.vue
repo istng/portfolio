ec17988acde7f94c73fc84a9d58fd232fc63384a
Iván Gk <ivanstng@gmail.com>
2021-06-14 16:51:04 -0300

    stylise credits card a little

--- src/components/CreditsCard.vue
+++ src/components/CreditsCard.vue
@@ -3,26 +3,28 @@
     <img class="back-thumbnail" :src='backflip'>
     <div class="back-credits">
       <div v-if="credits.type == 'short'">
-        <!--<strong>Dirección:</strong> {{ credits.directors.join(', ') }} <br>
-        <strong>Guión:</strong> {{ credits.writers.join(', ') }} <br>
-        <strong>Editores:</strong> {{ credits.editors.join(', ') }} <br>
-        -->
         <div v-if="credits.writers.length > 1">
+          <br>
           <strong>Realizado por:</strong> {{ credits.directors.join(', ') }} <br>
         </div>
         <div v-else>
-          <strong>Idea y guión original:</strong> {{ credits.writers.join(', ') }} <br>
+          <strong>Guión original:</strong> {{ credits.writers.join(', ') }} <br>
           <strong>Realizado por:</strong> {{ credits.directors.join(', ') }} <br>
         </div>
+        <br>
         <strong>Cast:</strong> {{ credits.cast.join(', ') }} <br>
         <div v-for="other in credits.others" :key="other[1]">
           <strong>{{ other[0] }}:</strong> {{ other[1].join(', ') }} <br>
         </div>
+        <br>
         <strong v-if="credits.awards.join(', ') != ''">Premios o menciones:</strong> {{ credits.awards.join(', ') }} <br>
       </div>
       <div v-if="credits.type == 'youtube'">
+        <br>
         <strong>Idea y edición:</strong> {{ credits.creators.join(', ') }} <br>
+        <br>
         <strong>Escenas:</strong> {{ credits.scenes.join(', ') }} <br>
+        <br>
         <strong>Música:</strong> {{ credits.sounds.join(', ') }} <br>
       </div>
     </div>
@@ -52,7 +54,7 @@ export default defineComponent({
 .back-thumbnail {
   width: 100%;
   border-radius: 25px;
-  opacity: 80%;
+  opacity: 15%;
   position: absolute;
   top: 0;
   left: 0;
@@ -60,6 +62,7 @@ export default defineComponent({
 .back-credits {
   position: absolute;
   left: 5%;
+  color: white;
 }
 .back-container {
   position: relative;
