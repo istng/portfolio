7677106f6b4d75bc5166fe691148ef752628c209
Iván Gk <ivanstng@gmail.com>
2021-06-15 14:52:18 -0300

    replace links with icons

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,39 +1,62 @@
 <template>
+    <div class="name-title-container">
+      <h1 class="name-title">Iván Guralnik</h1>
+    </div>
   <div class="header-container">
     <div>
-      <h2 class="name-title">Iván Guralnik</h2>
-      <p class="pompadur"> cine, ciencia, jazz, programación </p>
+      <p class="pompadur"> cine, jazz, programación </p>
     </div>
     <h3 class="contact">
-      <a class="contact-link" href="https://www.youtube.com/channel/UC0paZQh-P3ruttlq26UmNig" target=”_blank”>igk</a> <a class="contact-link" href="https://www.instagram.com/ivangk.gk/" target=”_blank”>@ivangk.gk</a> ivanstng@gmail.com
+      <a class="contact-link" href="https://www.youtube.com/channel/UC0paZQh-P3ruttlq26UmNig" target=”_blank”><font-awesome-icon :icon="['fab', 'youtube']"/></a> <a class="contact-link" href="https://www.instagram.com/ivangk.gk/" target=”_blank”><font-awesome-icon :icon="['fab', 'instagram']"/></a> <a href="mailto:ivanstng@gmail.com"><font-awesome-icon :icon="['far', 'envelope']"/></a>
     </h3>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+library.add(faInstagram);
+library.add(faYoutube);
+library.add(faEnvelope);
 
 export default defineComponent({
   name: 'Header',
   components: {
+    FontAwesomeIcon,
   },
+  setup() {
+    const copy = async () => {
+      await navigator.clipboard.writeText("ivanstng@gmail.com");
+    };
+    return {
+      copy,
+    };
+  }
 });
 </script>
 
 <style type="text/css">
-.name-title, .contact {
-  flex-grow: 1;
-  min-width: 25%;
-}
 .name-title {
+  margin-top: 3rem;
   color: white;
-  text-align: left;
+  text-align: center;
   margin-bottom: 0;
 }
+@media (max-width: 768px) {
+  .name-title {
+    margin-top: 1.9rem;
+  }
+}
 .pompadur {
-  margin-top: 0.4rem; 
+  margin-top: 0.4rem;
+  margin-bottom: 0.8rem;
 }
 .contact {
+  margin-top: 0.6rem; 
+  margin-bottom: 0.6rem; 
   color: #e9a44d;
   word-spacing: 0.8rem;
   text-align: right;
@@ -44,5 +67,10 @@ export default defineComponent({
 .header-container {
   display: flex;
   align-items: center;
+  flex-direction: column;
 }
+a, a:hover, a:visited, a:active {
+  color: inherit;
+  text-decoration: none;
+ }
 </style>
