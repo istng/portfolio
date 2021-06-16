97f8055b0bb34a2dd8bb1c5e9630d14f58d54e5e
Iván Gk <ivanstng@gmail.com>
2021-06-16 15:37:36 -0300

    preliminary bio

--- src/components/Bio.vue
+++ src/components/Bio.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <h4 class="bio-title">Bio</h4>
-    <p class="bio-body">Acá va la bio, y en inglés</p>
+    <div class="bio-body-container">
+      <p class="bio-body bio-body-spanish">Apasionado por el cine desde muy chico, alquilando cosntantemente VHS y siempre yendo al cine, terminé encontrando una admiración por las ciencias y la tecnología en series como Cosmos o Viaje a las Estrellas. Así, estudiando Ciencias de la Computación (Exactas, UBA), pero nunca dejando de lado el ver películas y cine, como tocar música con amigos. Llegando al fin de la carrera en Computación, encontré que mi verdadera pasión siempre estuvo en el cine y los medios audiovisuales.</p>
+      <p class="bio-body bio-body-english">Passionate for films since a kid, constantly renting VHS and always going to the movies, I ended up admiring the ciencies and technology in series like Cosmos or Star Trek. Thus, I studied Computer Science (Exacts Faculty, UBA), but never setting aside watching movies, as playing music with friends. Coming to the end of the Computer Science career, I found that my true passion always was on cinema and audiovisual mediums.</p>
+    </div>
   </div>
 </template>
 
@@ -22,6 +25,15 @@ export default defineComponent({
 }
 .bio-body {
   color: white;
-  text-align: center;
+  text-align: left;
+}
+.bio-body-container {
+  display: flex;
+  justify-content: space-around;
+}
+@media (max-width: 768px) {
+  .bio-body-container {
+    flex-direction: column;
+  }
 }
 </style>
