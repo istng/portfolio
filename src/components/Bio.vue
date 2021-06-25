f9c5e6f56d1c843c51c63c30ea7898649c25b156
Iván Gk <ivanstng@gmail.com>
2021-06-25 09:59:33 -0300

    update bio

--- src/components/Bio.vue
+++ src/components/Bio.vue
@@ -2,8 +2,8 @@
   <div>
     <h4 class="bio-title">Bio</h4>
     <div class="bio-body-container">
-      <p class="bio-body bio-body-spanish">Apasionado por el cine desde muy chico, alquilando cosntantemente VHS y siempre yendo al cine, terminé encontrando una admiración por las ciencias y la tecnología en series como Cosmos o Viaje a las Estrellas. Así, estudiando Ciencias de la Computación (Exactas, UBA), pero nunca dejando de lado el ver películas y cine, como tocar música con amigos. Llegando al fin de la carrera en Computación, encontré que mi verdadera pasión siempre estuvo en el cine y los medios audiovisuales.</p>
-      <p class="bio-body bio-body-english">Passionate for films since a kid, constantly renting VHS and always going to the movies, I ended up admiring the ciencies and technology in series like Cosmos or Star Trek. Thus, I studied Computer Science (Exacts Faculty, UBA), but never setting aside watching movies, as playing music with friends. Coming to the end of the Computer Science career, I found that my true passion always was on cinema and audiovisual mediums.</p>
+      <p class="bio-body bio-body-spanish">Apasionado por ver películas desde muy chico, el cine y el videoclub eran mi segunda casa. Terminé encontrando una admiración por las ciencias y la tecnología en series como Cosmos y Viaje a las Estrellas. Así, estudiando Ciencias de la Computación (Exactas, UBA), pero nunca dejando de lado mi amor por el septimo arte. Llegando al fin de la carrera en Computación, encontré que mi verdadera pasión siempre estuvo en el cine y los medios audiovisuales.</p>
+      <p class="bio-body bio-body-english">Passionate for films since a kid, the movies and video stores were my second home. I ended up admiring the ciencies and technology in series like Cosmos or Star Trek. Thus, I studied Computer Science (Exacts Faculty, UBA), but never setting aside my love for the seventh art. Coming to the end of the Computer Science career, I found that my true passion always was on cinema and audiovisual mediums.</p>
     </div>
   </div>
 </template>
