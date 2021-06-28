9f1b1d8f6f1109cd16480cd0f861244a540f4dde
Iván Gk <ivanstng@gmail.com>
2021-06-28 18:33:57 -0300

    update bio text

--- src/components/Bio.vue
+++ src/components/Bio.vue
@@ -2,8 +2,8 @@
   <div>
     <h4 class="bio-title">Bio</h4>
     <div class="bio-body-container">
-      <p class="bio-body" lang="es">Apasionado por ver películas desde muy chico, el cine y el videoclub eran mi segunda casa. Terminé encontrando una admiración por las ciencias y la tecnología en series como Cosmos y Viaje a las Estrellas. Así, estudiando Ciencias de la Computación (Exactas, UBA), pero nunca dejando de lado mi amor por el septimo arte. Llegando al fin de la carrera en Computación, encontré que mi verdadera pasión siempre estuvo en el cine y los medios audiovisuales.</p>
-      <p class="bio-body" lang="en">Passionate for films since a kid, the movies and video stores were my second home. I ended up admiring the ciencies and technology in series like Cosmos or Star Trek. Thus, I studied Computer Science (Exacts Faculty, UBA), but never setting aside my love for the seventh art. Coming to the end of the Computer Science career, I found that my true passion always was on cinema and audiovisual mediums.</p>
+      <p class="bio-body" lang="es">Apasionado por ver películas desde muy chico, el cine y el videoclub eran mi segunda casa. Terminé encontrando una admiración por las ciencias y la tecnología en series como Cosmos y Viaje a las Estrellas. Así, estudié Ciencias de la Computación (Exactas, UBA), pero nunca dejé de lado mi amor por el septimo arte. Llegando al fin de la carrera en Computación, encontré que mi verdadera pasión siempre estuvo en el cine y los medios audiovisuales.</p>
+      <p class="bio-body" lang="en">Passionate for films since a kid, the movies and video stores were my second home. I ended up admiring the sciences and technology in series like Cosmos or Star Trek. Thus, I studied Computer Science (Exacts Faculty, UBA), but I never set aside my love for the seventh art. Coming to the end of the Computer Science career, I found that my true passion always was on cinema and audiovisual mediums.</p>
     </div>
   </div>
 </template>
