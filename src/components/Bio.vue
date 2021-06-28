e478a4c52e1651eb1959447cd5ebcba772abee1c
Iván Gk <ivanstng@gmail.com>
2021-06-28 17:31:22 -0300

    fix bio by columns on desktop
    and add hyphens to break lines on text blocks

--- src/components/Bio.vue
+++ src/components/Bio.vue
@@ -2,8 +2,8 @@
   <div>
     <h4 class="bio-title">Bio</h4>
     <div class="bio-body-container">
-      <p class="bio-body bio-body-spanish">Apasionado por ver películas desde muy chico, el cine y el videoclub eran mi segunda casa. Terminé encontrando una admiración por las ciencias y la tecnología en series como Cosmos y Viaje a las Estrellas. Así, estudiando Ciencias de la Computación (Exactas, UBA), pero nunca dejando de lado mi amor por el septimo arte. Llegando al fin de la carrera en Computación, encontré que mi verdadera pasión siempre estuvo en el cine y los medios audiovisuales.</p>
-      <p class="bio-body bio-body-english">Passionate for films since a kid, the movies and video stores were my second home. I ended up admiring the ciencies and technology in series like Cosmos or Star Trek. Thus, I studied Computer Science (Exacts Faculty, UBA), but never setting aside my love for the seventh art. Coming to the end of the Computer Science career, I found that my true passion always was on cinema and audiovisual mediums.</p>
+      <p class="bio-body" lang="es">Apasionado por ver películas desde muy chico, el cine y el videoclub eran mi segunda casa. Terminé encontrando una admiración por las ciencias y la tecnología en series como Cosmos y Viaje a las Estrellas. Así, estudiando Ciencias de la Computación (Exactas, UBA), pero nunca dejando de lado mi amor por el septimo arte. Llegando al fin de la carrera en Computación, encontré que mi verdadera pasión siempre estuvo en el cine y los medios audiovisuales.</p>
+      <p class="bio-body" lang="en">Passionate for films since a kid, the movies and video stores were my second home. I ended up admiring the ciencies and technology in series like Cosmos or Star Trek. Thus, I studied Computer Science (Exacts Faculty, UBA), but never setting aside my love for the seventh art. Coming to the end of the Computer Science career, I found that my true passion always was on cinema and audiovisual mediums.</p>
     </div>
   </div>
 </template>
@@ -23,16 +23,30 @@ export default defineComponent({
   color: white;
   text-align: center;
 }
+.bio-body-container {
+  display: flex;
+}
 .bio-body {
   color: white;
   text-align: left;
+  flex-basis: 50%;
+  hyphens: auto;
 }
-.bio-body-container {
-  display: flex;
+.bio-body:not(:first-child) {
+  margin-left: 0.75rem;
+}
+.bio-body:not(:last-child) {
+  margin-right: 0.75rem;
 }
 @media (max-width: 768px) {
   .bio-body-container {
     flex-direction: column;
+  }
+  .bio-body:not(:first-child) {
+    margin-left: 0;
+  }
+  .bio-body:not(:last-child) {
+    margin-right: 0;
   }
 }
 </style>
