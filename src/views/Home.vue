a6f1aa1f27a77d11cc4cb0ae70c21429946dad16
Iván Gk <ivanstng@gmail.com>
2021-07-26 14:30:44 -0300

    abstract video and bio data as variables in corresponding files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
     <VideoCategory title="Cortos Publicados" :videos='publishedShorts' videosWidth="33%" categoryType="published"/>
     <VideoCategory title="Cortos Inéditos" :videos='unpublishedShorts' videosWidth="33%" categoryType="unpublished"/>
     <VideoCategory title="YouTube" :videos='youtubes' videosWidth="33%" categoryType="published"/>
-    <Bio />
+    <Bio :spanishText='spanishText' :englishText='englishText' />
     <Footer />
   </div>
 </template>
@@ -15,20 +15,11 @@ import Header from '@/components/Header.vue';
 import VideoCategory from '@/components/VideoCategory.vue';
 import Bio from '@/components/Bio.vue';
 import Footer from '@/components/Footer.vue';
-import nakatomi from '@/assets/nakatomi-drive.png';
-import obscured from '@/assets/obscured-by-sights.png';
-import notshop from '@/assets/not-in-the-shop.png';
-import nacherrestre from '@/assets/nacherrestre.png';
-import deshielo from '@/assets/deshielo.png';
-import galeria from '@/assets/galeria-cita.png';
-import frutilla from '@/assets/frutilla.png';
-import nakatomiblur from '@/assets/nakatomi-drive-blur.png';
-import obscuredblur from '@/assets/obscured-by-sights-blur.png';
-import notshopblur from '@/assets/not-in-the-shop-blur.png';
-import nacherrestreblur from '@/assets/nacherrestre-blur.png';
-import deshieloblur from '@/assets/deshielo-blur.png';
-import galeriablur from '@/assets/galeria-cita-blur.png';
-import frutillablur from '@/assets/frutilla-blur.png';
+/*custom variables to import*/
+import { publishedShorts } from '@/variables/videos.ts';
+import { unpublishedShorts } from '@/variables/videos.ts';
+import { youtubes } from '@/variables/videos.ts';
+import { spanishText, englishText } from '@/variables/bioText.ts';
 
 export default defineComponent({
   name: 'Home',
@@ -39,87 +30,12 @@ export default defineComponent({
     Footer,
   },
   setup() {
-    const publishedShorts = ref([
-      {thumbnail: deshielo, blurredThumbnail: deshieloblur, urlId: "k92daVzN_8Q", title: "Deshielo Acondicionado", 
-      credits: {
-        type: "short",
-        directors: ["Iván Guralnik", "Gabriela Velazquez"], 
-        writers: ["Iván Guralnik", "Gabriela Velazquez"], 
-        editors: ["Iván Guralnik", "Gabriela Velazquez"], 
-        cast: ["Franco Frizzo"], 
-        others: [],
-        awards: ["Cuarto lugar en la Octava Edición del Festival Internacional de Cine sobre el Trabajo."]
-        }
-      }
-    ]);
-    const unpublishedShorts = ref([
-      {thumbnail: nacherrestre, blurredThumbnail: nacherrestreblur, urlId: "", title: "Nacherrestre (Título Provisional)",
-      credits: {
-        type: "short",
-        directors: ["Iván Guralnik", "Gabriela Velazquez"], 
-        writers: ["Gabriela Velazquez", "Iván Guralnik"], 
-        editors: ["Iván Guralnik", "Gabriela Velazquez"], 
-        cast: ["Ignacio Mariotti"], 
-        others: [["Dirección Artística", ["Alan Lampert"]]],
-        awards: []
-        },
-        info: {sinopsis: "Una noche solitaria, un jóven presencia lo que podrían ser señales extraterrestres en su casa.", information: "Corto realizado de noche, principalmente con luz natural, rodado en una noche."},
-      }, 
-      {thumbnail: galeria, blurredThumbnail: galeriablur, urlId: "", title: "Galeria Boulevard (Título Provisional)", 
-      credits: {
-        type: "short",
-        directors: ["Iván Guralnik", "Gabriela Velazquez"], 
-        writers: ["Iván Guralnik", "Gabriela Velazquez"], 
-        editors: ["Iván Guralnik", "Gabriela Velazquez"], 
-        cast: ["Aldana Casarrubia", "Patricio Sabogal"], 
-        others: [["Diseño de Sonido", ["Aldana Casarrubia"]]],
-        awards: []
-        },
-        info: {sinopsis: "Una jóven recorre una galeria mientras espera encontrarse con su cita.", information: "Filmado en distintas galerias del barrio de Flores, CABA."},
-      }, 
-        {thumbnail: frutilla, blurredThumbnail: frutillablur, urlId: "", title: "Frutilla", 
-        credits: {
-          type: "short",
-          directors: ["Gabriela Velazquez", "Iván Guralnik"], 
-          writers: ["Gabriela Velazquez"], 
-          editors: ["Iván Guralnik", "Gabriela Velazquez"], 
-          cast: ["Aldana Casarrubia", "Elvis Alvarado"], 
-          others: [["Productor", ["David Xu Barco"]]],
-          awards: ["Aprobado para el ingreso a la ESCAC, Barcelona."]
-          },
-          info: {sinopsis: "Una fuerte discusión entre una pareja los lleva a reflexionar para sí mismos los lindos momentos que vivierons juntos.", information: "Escrito, rodado y editado en 24 horas como trabajo para ingreso a la ESCAC, Barcelona."},
-      }
-    ]);
-    const youtubes = ref([
-      {thumbnail: nakatomi, blurredThumbnail: nakatomiblur, urlId: "ve8gZ3UEMlU", title: "nakatomi drive", 
-      credits: {
-        type: "youtube",
-        creators: ["Iván Guralnik"], 
-        scenes: ["Die Hard, 1988"],
-        sounds: ["Future Shock, Herbie Hancock"]
-        }
-      }, 
-      {thumbnail: obscured, blurredThumbnail: obscuredblur, urlId: "NUVFjVWOpos", title: "obscured by sights", 
-      credits: {
-        type: "youtube",
-        creators: ["Iván Guralnik"], 
-        scenes: ["The Sopranos, 1999"],
-        sounds: ["Obscured By Clouds, Pink Floyd"]
-        }
-      },
-      {thumbnail: notshop, blurredThumbnail: notshopblur, urlId: "rIZyQmS3L3w", title: "not in the shop", 
-      credits: {
-        type: "youtube",
-        creators: ["Iván Guralnik"], 
-        scenes: ["Star Trek: Deep Space 9, 1993"],
-        sounds: ["Dragon Ball Z OST, Prologue & Subtitle I"]
-        }
-      }
-    ]);
     return {
       publishedShorts,
       unpublishedShorts,
       youtubes,
+      spanishText,
+      englishText,
     };
   }
 });
