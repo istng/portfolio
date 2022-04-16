fb96ade5d0a8dac91b3fbdd6ff4a2d24a65ddca0
istng <ivanstng@gmail.com>
2022-04-16 15:49:42 -0300

    add background to card and style it

--- src/components/UnpublishedVideo.vue
+++ src/components/UnpublishedVideo.vue
@@ -28,13 +28,30 @@ export default defineComponent({
 </script>
 
 <style type="text/css">
+.unpublished-card-container {
+  background-color: black;
+  opacity: 0.75;
+  padding: 100px;
+  padding-top: 78px;
+  border-radius: 10px;
+}
 .coming-soon-text {
   font-size: xx-large;
   text-shadow: 1px 1px black;
+  text-align: center;
+  padding-bottom: 20px;
+}
+.synopsis-text {
+  padding-bottom: 2px;
 }
 .synopsis-text,
 .information-text {
   font-size: x-large;
   text-shadow: 1px 1px black;
+}
+@media (max-width: 500px) {
+  .unpublished-card-container {
+    padding: 25px;
+  }
 }
 </style>
